ed5b28a914fa6bc6f5ba6ef0a9d08b90ed9142a9
Mert Aşan <mertemreasan@gmail.com>
2021-07-06 03:53:54 +0300

    add phpdoc for toBeCollection

--- src/Expectations.php
+++ src/Expectations.php
@@ -6,6 +6,9 @@ namespace Pest\Laravel;
 
 use Pest\Expectation;
 
+/*
+ * Asserts that the value is an instance of \Illuminate\Support\Collection
+ */
 expect()->extend('toBeCollection', function (): Expectation {
     // @phpstan-ignore-next-line
     return $this->toBeInstanceOf(\Illuminate\Support\Collection::class);
