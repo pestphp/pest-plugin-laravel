6d349048906d2937a1fa3af9ab3d2ad8f09b21d7
Mert Aşan <mertemreasan@gmail.com>
2021-07-09 11:50:35 +0300

    compatibility for and() method

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -6,5 +6,8 @@ namespace PHPSTORM_META {
     override(\expect(), map([
         '' => \Pest\Laravel\Contracts\ExpectationsInterface::class,
     ]));
+    override(\Pest\Expectation::and(0), map([
+        '' => \Pest\Laravel\Contracts\ExpectationsInterface::class,
+    ]));
 
 }
