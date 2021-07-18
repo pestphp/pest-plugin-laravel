e90982c80ac13631647a3da613830ea292c56b03
Bebeto Alves <bebeto.inbox@gmail.com>
2021-07-18 10:05:41 -0300

    add toHaveSessionErrors

--- src/Expectations.php
+++ src/Expectations.php
@@ -13,3 +13,17 @@ expect()->extend('toBeCollection', function (): Expectation {
     // @phpstan-ignore-next-line
     return $this->toBeInstanceOf(\Illuminate\Support\Collection::class);
 });
+
+/*
+ * Asserts that the request has session errors.
+ */
+expect()->extend('toHaveSessionErrors', function ($keys = [], mixed $format = null, string $errorBag = 'default'): Expectation {
+    // @phpstan-ignore-next-line
+    test()->expect($this->value)->toBeInstanceOf(\Illuminate\Testing\TestResponse::class);
+
+    // @phpstan-ignore-next-line
+    $this->value->assertSessionHasErrors($keys, $format, $errorBag);
+
+    // @phpstan-ignore-next-line
+    return $this;
+});
