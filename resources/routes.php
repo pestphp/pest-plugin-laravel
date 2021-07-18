e5c7fa0a3a63f338b241f067ba46343bdc1f5a51
Bebeto Alves <bebeto.inbox@gmail.com>
2021-07-18 10:04:32 -0300

    add post route to ContactsController

--- resources/routes.php
+++ resources/routes.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+Route::post('/contacts', \App\Http\Controllers\ContactsController::class)->name('contacts');
+
 Artisan::command('inspire', function () {
     $this->comment('pest');
 });
