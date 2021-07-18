7985e891e2c3ec8940cd23ee267a2de22258df5d
Bebeto Alves <bebeto.inbox@gmail.com>
2021-07-18 10:06:06 -0300

    add tests

---
+++ tests/Expect/toHaveSessionErrors.php
@@ -0,0 +1,46 @@
+<?php
+
+use function Pest\Laravel\post;
+use PHPUnit\Framework\ExpectationFailedException;
+
+beforeEach(function () {
+    $this->route = route('contacts');
+
+    $this->invalidData = [
+        'name'  => '',
+        'email' => '',
+    ];
+
+    $this->validData = [
+        'name'  => 'Elyneker',
+        'email' => 'elyneker@pest.com',
+    ];
+});
+
+test('pass', function () {
+    expect(post($this->route, $this->invalidData))->toHaveSessionErrors();
+
+    expect(post($this->route, array_merge($this->invalidData, ['email' => 'elyneker@pest.com'])))->toHaveSessionErrors('name');
+
+    expect(post($this->route, $this->invalidData))->toHaveSessionErrors(['name', 'email']);
+});
+
+test('not pass', function () {
+    expect(post($this->route, $this->validData))->not->toHaveSessionErrors();
+
+    expect(post($this->route, array_merge($this->validData, ['name' => ''])))->not->toHaveSessionErrors('email');
+
+    expect(post($this->route, $this->validData))->not->toHaveSessionErrors(['name', 'email']);
+});
+
+test('failure', function () {
+    expect(post($this->route, $this->validData))->toHaveSessionErrors();
+})->throws(ExpectationFailedException::class);
+
+test('not failure', function () {
+    expect(post($this->route, $this->invalidData))->not->toHaveSessionErrors();
+})->throws(ExpectationFailedException::class);
+
+test('failure if the error key is incorrect', function () {
+    expect(post($this->route, array_merge($this->invalidData, ['name' => 'Elyneker'])))->toHaveSessionErrors('name');
+})->throws(ExpectationFailedException::class);
