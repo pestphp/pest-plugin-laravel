45a5fa9bd5eec7525652faa94489c51f526ea161
Fabio Ivona <fabio.ivona@defstudio.it>
2021-10-01 11:36:25 +0200

    fix tests

--- tests/Database.php
+++ tests/Database.php
@@ -1,16 +1,15 @@
 <?php
 
-use Tests\TestCase;
 use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\assertModelExists;
 use function Pest\Laravel\assertModelMissing;
 use Tests\Models\User;
+use Tests\TestCase;
 
 assertDatabaseMissing('users', ['id' => 1]);
 
 test('assert model missing', function () {
-
-    if(!method_exists(TestCase::class, 'assertModelExists')){
+    if (!method_exists(TestCase::class, 'assertModelExists')) {
         $this->markTestSkipped('assertModelExist not supported for this laravel version');
     }
 
@@ -25,8 +24,7 @@ test('assert model missing', function () {
 });
 
 test('assert model exists', function () {
-
-    if(!method_exists(TestCase::class, 'assertModelExists')){
+    if (!method_exists(TestCase::class, 'assertModelExists')) {
         $this->markTestSkipped('assertModelExist not supported for this laravel version');
     }
 
