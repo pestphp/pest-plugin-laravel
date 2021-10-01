96302eb4d63f33966f2e8442ba1ef26ecfc671eb
Fabio Ivona <fabio.ivona@defstudio.it>
2021-10-01 10:30:13 +0200

    adds tests

--- tests/Database.php
+++ tests/Database.php
@@ -1,5 +1,29 @@
 <?php
 
-use function Pest\Laravel\{assertDatabaseMissing};
+use function Pest\Laravel\assertDatabaseMissing;
+use function Pest\Laravel\assertModelExists;
+use function Pest\Laravel\assertModelMissing;
+use Tests\Models\User;
 
 assertDatabaseMissing('users', ['id' => 1]);
+
+test('assert model missing', function () {
+    $user = User::make([
+        'name'     => 'test user',
+        'email'    => 'email@test.xx',
+        'password' => Hash::make('password'),
+    ]);
+    $user->id = 1;
+
+    assertModelMissing($user);
+});
+
+test('assert model exists', function () {
+    $user = User::create([
+        'name'     => 'test user',
+        'email'    => 'email@test.xx',
+        'password' => Hash::make('password'),
+    ]);
+
+    assertModelExists($user);
+});
